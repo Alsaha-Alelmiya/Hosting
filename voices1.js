efeaf04e436c48e7b8e92a5c02a77e8d147d0259
Alsaha-Alelmiya <119962189+Alsaha-Alelmiya@users.noreply.github.com>
2023-02-01 23:45:55 +0300

    Add files via upload

---
+++ voices1.js
@@ -0,0 +1,141 @@
+console.log("After Reading Go Here!")
+
+var ar1 = new Audio("https://files.soundoftext.com/47b0eb70-5823-11ed-a44a-8501b7b1aefa.mp3");
+
+var ar2 = new Audio("https://files.soundoftext.com/1b120080-a0db-11ed-a44a-8501b7b1aefa.mp3");
+
+var ar3 = new Audio("https://files.soundoftext.com/20745410-a0db-11ed-a44a-8501b7b1aefa.mp3");
+
+var ar4 = new Audio("https://files.soundoftext.com/257ca0c0-a0db-11ed-a44a-8501b7b1aefa.mp3");
+
+var ar5 = new Audio("https://files.soundoftext.com/2927a760-a0db-11ed-a44a-8501b7b1aefa.mp3");
+
+var ar6 = new Audio("https://files.soundoftext.com/2d23db40-a0db-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Nouns Go Here!");
+
+var n1 = new Audio("https://files.soundoftext.com/b475f4f0-641a-11ed-a44a-8501b7b1aefa.mp3");
+
+var n2 = new Audio("https://files.soundoftext.com/5a50fe80-4b14-11ed-a44a-8501b7b1aefa.mp3");
+
+var n3 = new Audio("https://files.soundoftext.com/b2720540-57fe-11ed-a44a-8501b7b1aefa.mp3");
+
+var n4 = new Audio("https://files.soundoftext.com/5c0c7900-4715-11ed-a4bb-5deaa2337781.mp3");
+
+var n5 = new Audio("https://files.soundoftext.com/df5f8980-4d0c-11ed-a44a-8501b7b1aefa.mp3");
+
+var n6 = new Audio("https://files.soundoftext.com/175e0a10-55f9-11ed-a44a-8501b7b1aefa.mp3");
+
+var n7 = new Audio("https://files.soundoftext.com/f8466270-90eb-11ed-a44a-8501b7b1aefa.mp3");
+
+var n8 = new Audio('https://files.soundoftext.com/6bef1aa0-5db1-11ed-a44a-8501b7b1aefa.mp3');
+
+			var n9 = new Audio('https://files.soundoftext.com/daa83ac0-4b92-11ed-a44a-8501b7b1aefa.mp3');
+			
+			var n10 = new Audio("https://files.soundoftext.com/a06ac190-5f40-11ed-a44a-8501b7b1aefa.mp3");
+			
+			var n11 = new Audio("https://files.soundoftext.com/2eb00480-6359-11ed-a44a-8501b7b1aefa.mp3");
+			
+console.log("Verbs Go Here!");
+
+var v1 = new Audio("https://files.soundoftext.com/a6cfbc10-4b8c-11ed-a44a-8501b7b1aefa.mp3");
+
+var v2 = new Audio("https://files.soundoftext.com/850a58b0-51bd-11ed-a44a-8501b7b1aefa.mp3");
+
+var v3 = new Audio("https://files.soundoftext.com/3e5dbd50-5dc3-11ed-a44a-8501b7b1aefa.mp3");
+
+var v4 = new Audio("https://files.soundoftext.com/db2f33c0-59e5-11ed-a44a-8501b7b1aefa.mp3");
+
+var v5 = new Audio("https://files.soundoftext.com/201a5e20-9c02-11ed-a44a-8501b7b1aefa.mp3");
+
+var v6 = new Audio("https://files.soundoftext.com/afc132d0-575e-11ed-a44a-8501b7b1aefa.mp3");
+
+var v7 = new Audio("https://files.soundoftext.com/d95402e0-4ea7-11ed-a44a-8501b7b1aefa.mp3");
+
+var v8 = new Audio("https://files.soundoftext.com/2a5f9c10-9c02-11ed-a44a-8501b7b1aefa.mp3");
+
+var v9 = new Audio("https://files.soundoftext.com/a17f4c70-4ad4-11ed-a44a-8501b7b1aefa.mp3");
+
+var v10 = new Audio("https://files.soundoftext.com/95e760e0-4b02-11ed-a44a-8501b7b1aefa.mp3");
+
+var v11 = new Audio("https://files.soundoftext.com/3b88eb90-9c02-11ed-a44a-8501b7b1aefa.mp3");
+
+var v12 = new Audio("https://files.soundoftext.com/5c660500-4d47-11ed-a44a-8501b7b1aefa.mp3");
+
+var v13 = new Audio("https://files.soundoftext.com/40b1dfa0-9c02-11ed-a44a-8501b7b1aefa.mp3");
+
+var v14 = new Audio("https://files.soundoftext.com/444bf650-9c02-11ed-a44a-8501b7b1aefa.mp3");
+
+var v15 = new Audio("https://files.soundoftext.com/350c65f0-9421-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Verbs For Injuries Go Here!");
+
+var vfi1 = new Audio("https://files.soundoftext.com/167b4530-4a06-11ed-a44a-8501b7b1aefa.mp3");
+
+var vfi2 = new Audio("https://files.soundoftext.com/0dae6540-4d0d-11ed-a44a-8501b7b1aefa.mp3");
+
+var vfi3 = new Audio("https://files.soundoftext.com/1074a010-47fd-11ed-a4bb-5deaa2337781.mp3");
+
+var vfi4 = new Audio("https://files.soundoftext.com/b72a2990-46fc-11ed-a4bb-5deaa2337781.mp3");
+
+var vfi5 = new Audio("https://files.soundoftext.com/ccc481f0-5bfc-11ed-a44a-8501b7b1aefa.mp3");
+
+var vfi6 = new Audio("https://files.soundoftext.com/0bf6d190-4d0a-11ed-a44a-8501b7b1aefa.mp3");
+
+var vfi7 = new Audio("https://files.soundoftext.com/38a1f880-4d43-11ed-a44a-8501b7b1aefa.mp3");
+
+var vfi8 = new Audio("https://files.soundoftext.com/f52e1ed0-5828-11ed-a44a-8501b7b1aefa.mp3");
+
+var vfi9 = new Audio("https://files.soundoftext.com/064433c0-4d0d-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Adjectives Go Here!");
+
+var adj1 = new Audio("https://files.soundoftext.com/de976640-5459-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj2 = new Audio("https://files.soundoftext.com/7c485370-90f7-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj3 = new Audio("https://files.soundoftext.com/b7a92f10-588b-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj4 = new Audio("https://files.soundoftext.com/9cd75ed0-a171-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj5 = new Audio("https://files.soundoftext.com/484b90d0-58ff-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj6 = new Audio("https://files.soundoftext.com/417c0a60-53a9-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj7 = new Audio("https://files.soundoftext.com/afe46940-506e-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj8 = new Audio("https://files.soundoftext.com/aff3f2d0-a171-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Reflexive-Pronouns Go Here!")
+
+var rp1 = new Audio("https://files.soundoftext.com/2a811640-a172-11ed-a44a-8501b7b1aefa.mp3");
+
+var rp2 = new Audio("https://files.soundoftext.com/2d079a10-a172-11ed-a44a-8501b7b1aefa.mp3");
+
+var rp3 = new Audio("https://files.soundoftext.com/2fc07880-a172-11ed-a44a-8501b7b1aefa.mp3");
+
+var rp4 = new Audio("https://files.soundoftext.com/3fec0a30-62c0-11ed-a44a-8501b7b1aefa.mp3");
+
+var rp5 = new Audio("https://files.soundoftext.com/34938460-a172-11ed-a44a-8501b7b1aefa.mp3");
+
+var rp6 = new Audio("https://files.soundoftext.com/2c236370-6f10-11ed-a44a-8501b7b1aefa.mp3");
+
+var rp7 = new Audio("https://files.soundoftext.com/d1bad460-7ec6-11ed-a44a-8501b7b1aefa.mp3");
+
+var rp8 = new Audio("https://files.soundoftext.com/3f7ffed0-a172-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Conjunctions Go Here!");
+
+var con1 = new Audio("https://files.soundoftext.com/f4cf7640-4a41-11ed-a44a-8501b7b1aefa.mp3");
+
+var con2 = new Audio("https://files.soundoftext.com/fbab2870-4946-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Real Talk Go Here");
+
+var rt1 = new Audio("https://files.soundoftext.com/8880cec0-a172-11ed-a44a-8501b7b1aefa.mp3");
+
+var rt2 = new Audio("https://files.soundoftext.com/8a4dc690-a172-11ed-a44a-8501b7b1aefa.mp3");
+
+var rt3 = new Audio("https://files.soundoftext.com/8f197f70-a172-11ed-a44a-8501b7b1aefa.mp3");
+
+var rt4 = new Audio("https://files.soundoftext.com/932e9280-a172-11ed-a44a-8501b7b1aefa.mp3");
